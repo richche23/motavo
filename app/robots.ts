382a70958f15e85035a384740dfd76a7e7790597
richche23 <80568105+richche23@users.noreply.github.com>
2026-06-10 12:29:40 +0900

    Add files via upload

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,12 @@
 import type { MetadataRoute } from 'next';
+import { SITE_URL } from '@/lib/seo';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://www.motavo.com.au/sitemap.xml',
-    host: 'https://www.motavo.com.au',
+    rules: [
+      { userAgent: '*', allow: '/', disallow: ['/api/'] },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
   };
 }
